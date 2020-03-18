d721b0392d0c2d980a23fed0e50e4d43d5c5ddc1
yan <1277218464@qq.com>
2020-03-18 23:15:48 +0800

    修改分页

--- util/pageHelper.js
+++ util/pageHelper.js
@@ -30,10 +30,14 @@ async function totalNum(tableName, limited) {
  * @returns {number} 页码数
  */
 function pageSize(totalNum, count) {
+    let number = parseInt(count);
+    if (totalNum > number) {
+        return 1;
+    }
     if (totalNum == 0) {
         return 0;
     }
-    return count % totalNum;
+    return number % totalNum;
 }
 
 
@@ -46,11 +50,23 @@ function pageSize(totalNum, count) {
  */
 async function page(page, count, param, tableName, suffix) {
     let promise = await totalNum(tableName, suffix);
-    let number = await pageSize(promise[0].total, count);
+    let number;
+    let total = promise[0].total;
+    let countNum = parseInt(count);
+    if (total == 0) {
+        number = 0;
+    } else if (countNum > total) {
+        number = 1
+    } else if (countNum < total) {
+        if (total % countNum == 0) {
+            number = total / countNum;
+        } else {
+            number = total / countNum + 1;
+        }
+    }
     if (number > 0) {
-        page = page + 1;
         let sql = "select " + param + " from " + tableName + suffix
-            + " limit " + count + " offset " + page;
+            + " limit " + count + " offset " + parseInt(page);
         let data = await mysql.query(sql);
         return data;
     }
